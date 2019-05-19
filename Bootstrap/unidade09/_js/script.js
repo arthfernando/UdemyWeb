3df369e6d103ddd8c8f09afc35db5bd28aa881ee
Arthur Bitencourt <arthur.fernando03@gmail.com>
2019-05-19 18:51:07 -0300

    Bootstrap & ECMAScript

---
+++ Bootstrap/unidade09/_js/script.js
@@ -0,0 +1,7 @@
+$(function() {
+	$('.carousel').carousel({
+		interval: 2000, //2 segundos
+		pause: "null", //mouse em cima nao pausa o slide
+		// wrap: false, //evita que volte pro inicio quando chega no fim
+	});
+});
